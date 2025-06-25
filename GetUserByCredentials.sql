2facd61610710289a99e61c49084c600b170c2bc
anandav <anand.av@hotmail.com>
2025-06-25 11:32:38 +0530

    Refactor GetUserByCredentials procedure to clean up comments and improve readability

--- GetUserByCredentials.sql
+++ GetUserByCredentials.sql
@@ -1,18 +1,14 @@
 CREATE PROCEDURE GetUserByCredentials
     @Username NVARCHAR(50),
-    @Password NVARCHAR(256) -- Adjust size according to hash length
+    @Password NVARCHAR(256)
 AS
 BEGIN
     SET NOCOUNT ON;
-
     DECLARE @PasswordHash VARBINARY(64); -- Example for SHA-256
-
-    -- Generate the hash of the input password using a function similar to HASHBYTES
     SELECT @PasswordHash = HASHBYTES('SHA2_256', @Password);
-
-    -- Query the user table with the hashed password
     SELECT UserId, Username, Email, FirstName, LastName
     FROM Users
     WHERE Username = @Username AND PasswordHash = @PasswordHash;
+
 END
 GO
