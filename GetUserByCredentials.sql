f6f6f6277fd48318bc231f7190abc69ef1847376
Anand AV <anand.av@hotmail.com>
2025-06-25 11:40:20 +0530

    Update GetUserByCredentials.sql
    Password to PasswordHash

--- GetUserByCredentials.sql
+++ GetUserByCredentials.sql
@@ -1,11 +1,11 @@
 CREATE PROCEDURE GetUserByCredentials
     @Username NVARCHAR(50),
-    @Password NVARCHAR(256)
+    @PasswordHash NVARCHAR(256),
 AS
 BEGIN
     SET NOCOUNT ON;
     DECLARE @PasswordHash VARBINARY(64); -- Example for SHA-256
-    SELECT @PasswordHash = HASHBYTES('SHA2_256', @Password);
+    SELECT @PasswordHash = HASHBYTES('SHA2_256', @PasswordHash);
     SELECT UserId, Username, Email, FirstName, LastName
     FROM Users
     WHERE Username = @Username AND PasswordHash = @PasswordHash;
